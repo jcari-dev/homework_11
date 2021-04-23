e3893c2d61f361dbc0a7c3bc6da8e0a50cef355d
jcari-rgb <582119jc@gmail.com>
2021-04-23 16:27:46 -0400

    added new vampires

--- models/vampire.js
+++ models/vampire.js
@@ -4,19 +4,17 @@ const Schema = mongoose.Schema;
 const vampireSchema = new Schema({
   name: { type: String, required: true, unique: true },
   title:{type: String, default: 'Count'},
-  hair_color:{type: String, default: 'N/D'},
+  hair_color:{type: String, default: 'blonde'},
   eye_color:{type: String, default: 'N/D' },
   dob: {type: Date, default:"Unknown"},
-  loves: {type: Array, default: "Blood and Dark Places"},
+  loves: {type: Array, default: ["Blood", "Dark Places"]},
   location: String,
   gender: String,
   victims: {type: Number, minimum: 1}
   // add your code here to set up your schema
-},{timestamps: true});
+},{timestamps: true},{ strict: true });
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
 
 //make this exportable to be accessed in `app.js`
 module.exports = Vampire;
-
-
