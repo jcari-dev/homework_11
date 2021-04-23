66b272580c40877fde8b0edf3547a325ccd1ad9f
jcari-rgb <582119jc@gmail.com>
2021-04-23 12:08:58 -0400

    added gitignore to make your life easier, also now the collection displays the data properly

---
+++ app.js
@@ -0,0 +1,32 @@
+/**********************************************************************
+Set up and Configuration
+**********************************************************************/
+// Dependencies
+const mongoose = require('mongoose');
+const seedData = require('./models/seed_vampires.js');
+const Vampire = require('./models/vampire.js');
+
+// Configuration
+const mongoURI = 'mongodb://localhost:27017/'+ 'vampires';
+const db = mongoose.connection;
+
+// Connect to Mongo
+mongoose.connect( mongoURI );
+
+// Connection Error/Success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', mongoURI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+db.on( 'open' , ()=>{
+  console.log('Connection made!');
+});
+
+/**********************************************************************
+Write Your Code Below
+**********************************************************************/
+// Vampire.insertMany(seedData,(err, vampires) => {
+//   if (err){ console.log(err)}
+//     console.log("added provided vampire data", vampires)
+//     mongoose.connection.close();
+//   });
